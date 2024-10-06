e58cef2e42405be855de31220645ce9fe0ef6172
nabil-rady <midorady9999@gmail.com>
2024-10-06 03:14:53 +0300

    Add my orders endpoint.

---
+++ app/Http/Controllers/UserOrderController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+
+use OpenApi\Attributes as OA;
+
+class UserOrderController extends Controller implements HasMiddleware
+{
+    public static function middleware()
+    {
+        return [new Middleware('auth:sanctum')];
+    }
+
+
+    #[OA\Get(
+        path: "/api/my-orders",
+        operationId: "myOrders",
+        tags: ['Orders'],
+        security: [
+            [
+                'sanctum' => [],
+            ]
+        ],
+        description: "Returns orders made by the authenticated user"
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Successful"
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthenticated"
+    )]
+    public function __invoke(Request $request)
+    {
+        return $request->user()->load('orders.products');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,15 @@ use App\Http\Controllers\AuthAPIController;
 use App\Http\Controllers\CategoryAPIController;
 use App\Http\Controllers\PlaceOrderController;
 use App\Http\Controllers\ProductAPIController;
-
+use App\Http\Controllers\UserOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthAPIController::class, 'register']);
 Route::post('login', [AuthAPIController::class, 'login']);
 
-Route::post('order', [PlaceOrderController::class, '__invoke']);
+Route::post('order', PlaceOrderController::class);
+Route::get('my-orders', UserOrderController::class);
 
 Route::apiResource('category', CategoryAPIController::class);
 Route::apiResource('product', ProductAPIController::class);
